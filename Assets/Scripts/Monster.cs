14dc816fbeca1b4ff452e2d6fc779e7cfd593ed8
yebinnnnn <114918259+yebinnnnn@users.noreply.github.com>
2023-07-09 15:06:00 +0900

    0709

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,54 +1,30 @@
-//종스크롤 슈팅게임 몬스터 코드 참고함 
+//몬스터 생성코드 참고
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : Monobehaviour
+public class Monster : MonoBehaviour
 { //몬스터에서 컬러속성 어케할건지....
-    public float speed;
-    public int health;
+    public float velocity;
+    public int hp = 1;
     public int color;
-    public Sprite[] sprites;
-
-    Rigidbody2D rigid;
 
     void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
-        rigid.velocity = Vector2.right * speed;
+        rigid.velocity = new Vector(-1,rigid,velocity.y);
     }
 
     void OnHit(int color)
     {
-        if (0)
+        if (color==this.color)
         {
-            health -= 1;
-            spriteRenderer.sprite = sprites[1];
-            invoke("ReturnSprite", 0.1f);
-
-            if (health <= 0)
+            hp -= 1;
+            if (hp <= 0)
             {
                 Destroy(gameObject);
             }
         }
     }
-    void ReturnSprite()
-    {
-        spriteRenderer.sprite = sprites[0];
-    }
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "BorderBullet") //여기선 맵 끝(border bullet)에 도달시 적이 파괴되는 코드로 작성했지만 추가로 주인공에게 데미지가 들어가야함
-        {
-            Destroy(gameObject);
-        }
-        elseif(collision.gameObject.tag == "PlayerBullet"){
-            Bullet bullet = collision.gameObject.GetComponent<bullet>();
-            OnHit(bullet.dmg);
-            Destroy(collision.gameObject);
-        }
-    }
-
 };
